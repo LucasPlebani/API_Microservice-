3475e30e979aeef46acf9dc3eacf99cff1192122
Marie-France Fritz <mariefrance.fritz@outlook.fr>
2025-06-03 14:16:42 +0200

    séparation du frontend

---
+++ authMicroService/src/models/TokenModels.js
@@ -0,0 +1,25 @@
+const { generateDeviceFingerprint } = require("../../utils/securityUtils");
+const { user } = require("./UserModels");
+
+async function generateTokenPayload(req) {
+  if (!user) {
+    throw new Error("Utilisateur non trouvé");
+  }
+
+  // Création des propriétés du payload du token
+  const tokenPayload = {
+    userId: user._id.toString(),
+    role: "user",
+    issueAt: Date.now(),
+    expiresIn: Date.now() + 900 * 1000,
+    nonce: 0,
+    proofOfWork: "",
+    scope: ["read", "write"],
+    issuer: "authServer",
+    deviceFingerprint: generateDeviceFingerprint(req),
+  };
+
+  return tokenPayload;
+}
+
+module.exports = generateTokenPayload;
